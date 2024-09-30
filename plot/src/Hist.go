46fa140d01b725d6e540ecde77aa0caefef928cf
Anton Nesterov <anton@demiurg.io>
2024-09-30 11:55:13 +0200

    png plots template

--- plot/src/Hist.go
+++ plot/src/Hist.go
@@ -16,16 +16,28 @@ import (
 
 func HistPlot(this js.Value, args []js.Value) interface{} {
 	var (
-		values = args[0]
-		bins   = args[1]
-		opts   js.Value
-		text   = "Histogram"
+		values    = args[0]
+		bins      = args[1]
+		opts      js.Value
+		text      = "Histogram"
+		lineColor = color.RGBA{R: 255, G: 255, B: 255, A: 255}
+		fillColor = color.RGBA{R: 50, G: 50, B: 50, A: 255}
+		normColor = color.RGBA{R: 255, G: 0, B: 0, A: 255}
 	)
 
 	if len(args) == 3 {
 		opts = args[2]
 		if !opts.Get("title").IsUndefined() {
 			text = opts.Get("title").String()
+		}
+		if !opts.Get("lineColor").IsUndefined() {
+			lineColor = HexToRGBA(opts.Get("lineColor").String())
+		}
+		if !opts.Get("fillColor").IsUndefined() {
+			fillColor = HexToRGBA(opts.Get("fillColor").String())
+		}
+		if !opts.Get("normColor").IsUndefined() {
+			normColor = HexToRGBA(opts.Get("normColor").String())
 		}
 	}
 
@@ -39,10 +51,12 @@ func HistPlot(this js.Value, args []js.Value) interface{} {
 	if err != nil {
 		panic(err)
 	}
+	h.LineStyle.Color = lineColor
+	h.FillColor = fillColor
 	h.Normalize(1)
 	p.Add(h)
 	norm := plotter.NewFunction(distuv.UnitNormal.Prob)
-	norm.Color = color.RGBA{R: 255, A: 255}
+	norm.Color = normColor
 	norm.Width = vg.Points(2)
 	p.Add(norm)
 	writer, err := p.WriterTo(6*vg.Inch, 4*vg.Inch, "png")
--- plot/src/utils.go
+++ plot/src/utils.go
@@ -6,6 +6,8 @@ package src
 import (
 	"bytes"
 	"encoding/base64"
+	"fmt"
+	"image/color"
 	"io"
 )
 
@@ -13,4 +15,24 @@ func WriterToBase64String(writer io.WriterTo) string {
 	var buf bytes.Buffer
 	writer.WriteTo(&buf)
 	return base64.StdEncoding.EncodeToString(buf.Bytes())
+}
+
+func HexToRGBA(hex string) color.RGBA {
+	var c color.RGBA
+	switch len(hex) {
+	case 4:
+		fmt.Sscanf(hex, "#%1x%1x%1x%1x", &c.R, &c.G, &c.B, &c.A)
+		c.R *= 17
+		c.G *= 17
+		c.B *= 17
+		c.A = 255
+	case 7:
+		fmt.Sscanf(hex, "#%02x%02x%02x", &c.R, &c.G, &c.B)
+		c.A = 255
+	case 9:
+		fmt.Sscanf(hex, "#%02x%02x%02x%02x", &c.R, &c.G, &c.B, &c.A)
+	default:
+		fmt.Println("Invalid hex color:", hex)
+	}
+	return c
 }
